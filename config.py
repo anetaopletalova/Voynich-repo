328c94ab8b3645d16b95a65456c72272994b4ea7
Aneta Opletalova <aneta.opletalova@sde.cz>
2022-03-21 16:38:28 +0100

    update database url

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # 'postgresql+psycopg2://postgres:admin@localhost:5432/Voynich'
 # postgresql+psycopg2://postgres:postgres@localhost:12345/voynichdb
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite://")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite://").replace('postgres://', 'postgresql://')
     SQLALCHEMY_ECHO = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'RaGMe3V87xh6HLhb1+IWVKUnW5Byn1+Pf0jMMD7q7XwpCl2F/r4AhPpp95zMpFzqktrLd2ka5j' \
